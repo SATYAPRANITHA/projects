d122798ff8d5782e88a34faa65427a3d1ef7ea1c
SATYAPRANITHA <satyapranitha212@gmail.com>
2023-11-26 12:22:24 +0530

    changes

---
+++ login-signup/frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from './pages/signup';
+import Login from './pages/login';
+
+function App(){
+  return(
+    <div className="App">
+      <Signup/>
+      <Login/>
+      
+    
+
+    </div>
+  )
+}
+export default App
